ace49eee4a81f4d5beeb777e58a86dd9c217fe89
mattak <mattak.me@gmail.com>
2014-10-25 00:42:29 +0900

    Initial Commit

---
+++ SceneKitTraining/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SceneKitTraining
+//
+//  Created by Mattak on 2014/10/25.
+//  Copyright (c) 2014年 Mattak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
